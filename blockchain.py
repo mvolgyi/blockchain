80683a5f66660670458d2bdacb699f7392eb04bf
mvolgyi <you@example.com>
2018-08-22 19:14:34 -0300

    commiting the changes in the blockchain file

---
+++ blockchain.py
@@ -0,0 +1,32 @@
+# Initializing our blockchain list
+blockchain = []
+
+def get_last_blockchain_value():
+    """ Returns the last value of the current blockchain """
+    return blockchain[-1]
+
+
+def add_value(transaction_amount, last_transaction=[1]):
+    """ Append a new value as well as the last blockchanin value to the blockchain
+        Arguemnts:
+            :transaction_amount: The amount that should be added.
+            :last_transaction: The last blockchain transaction (default [1]).
+    """
+    blockchain.append([last_transaction, transaction_amount])
+
+
+def get_user_input():
+    """ Returns the input of the user (a new transaction amount) as a float. """
+    return float(input('Your transaction amount please: '))
+
+
+tx_amount = get_user_input()
+add_value(tx_amount)
+
+tx_amount = get_user_input()
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
+
+tx_amount = get_user_input()
+add_value(tx_amount, get_last_blockchain_value())
+
+print(blockchain)
