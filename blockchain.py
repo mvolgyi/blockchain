47ca48aa1d2bbd2a28e792461bd286c90444ddf0
Matias Volgyi <mvolgyi@opcionseguros.com.ar>
2018-08-29 10:42:17 -0300

    adding security measueres

--- blockchain.py
+++ blockchain.py
@@ -34,24 +34,34 @@ def print_blockchain_elements():
     for block in blockchain:
         print(" Outputtin Block")
         print(block)
+    else:
+        print('_' * 20)
 
 
 def verify_chain():
-    block_index = 0
+    # block_index = 0
     is_valid = True
-    for block in blockchain:
-        if block_index == 0:
-            block_index += 1
+    for block_index in range(len(blockchain)):
+        if  block_index == 0:
             continue
-        if block[0] == blockchain[block_index -1]:
+        elif blockchain[block_index] [0] == blockchain[block_index -1]:
             is_valid = True
         else:
             is_valid = False
-            break
+        # if block_index == 0:
+        #     block_index += 1
+        #     continue
+        # if block[0] == blockchain[block_index -1]:
+        #     is_valid = True
+        # else:
+        #     is_valid = False
+        #     break
+        # block_index += 1
     return is_valid
 
+waitin_for_input = True
 
-while True:
+while waitin_for_input:
     print("Please choose")
     print("1: Add a new transaction value")
     print("2: Output the blockchain blocks")
@@ -64,15 +74,17 @@ while True:
     elif user_choice == '2':
         print_blockchain_elements()
     elif  user_choice == 'q':
-        break
+        waitin_for_input = False
     elif user_choice == 'h':
         if len(blockchain) >= 1:
             blockchain[0] = [2]
     else:
         print("Input was invalid, pleazse pick a value from the list!")
     if not verify_chain():
-        
+        print_blockchain_elements()
         print("Invalid Blockchain!")
         break
+else:
+    print("User left!")
 
 print("Done!")
